ec07c20b361f28b8e29aeb0a5a8b51d384c5298a
Mityoung <mityun.art@mail.ru>
2020-09-17 15:09:53 +0300

    [2-1] sum salaries

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,15 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
+
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number') {
+      sum += salaries[key];
+    } else {
+      return sum;
+    }
+  }
+  return sum;
 }
